9fb99569eacf88831bfdc84b7475fdc44b4d7a1d
Bennett Lewis <bennettwlewis@hotmail.com>
2022-12-23 21:30:26 -0600

    Boom, Day 2

--- D2/src/main/scala/main.scala
+++ D2/src/main/scala/main.scala
@@ -2,12 +2,15 @@ import akka.actor.Actor
 import akka.actor.{ActorRef, ActorSystem, PoisonPill, Props}
 import scala.collection.mutable.ListBuffer
 
-case class Game(opponent_move:Int,suggested_move:Int,line_num:Int)
+case class Game_1(opponent_move:Int,suggested_move:Int,line_num:Int)
+case class Game(opponent_move:Int,target_outcome:Char,line_num:Int)
 
 object Outcome_Evaluator{
   def props(accumulator:ActorRef):Props = Props(new Outcome_Evaluator(accumulator));
 }
-
+object Outcome_Decider{
+  def props(accumulator:ActorRef):Props = Props(new Outcome_Decider(accumulator));
+}
 object Formatter{
   def props(accumulator:ActorRef):Props = Props(new Formatter(accumulator));
 }
@@ -15,10 +18,10 @@ object Formatter{
 class Outcome_Evaluator(accumulator:ActorRef) extends Actor{
 
   override def receive: Receive = {
-    case g:Game => r_Game(g)
+    case g:Game_1 => r_Game(g)
   }
 
-  private def r_Game(game: Game): Unit = {
+  private def r_Game(game: Game_1): Unit = {
     val op_move = game.opponent_move
     val sug_move = game.suggested_move
     //Draw = 3
@@ -68,9 +71,63 @@ class Outcome_Evaluator(accumulator:ActorRef) extends Actor{
 
 }
 
+class Outcome_Decider(accumulator:ActorRef) extends Actor{
+  override def receive: Receive = {
+    case g: Game => r_Game(g)
+  }
+
+  private def r_Game(g: Game): Unit = {
+    var score = 0;
+    if (g.target_outcome == 'W')
+      score = do_Win(g.opponent_move)
+    else if (g.target_outcome == 'D')
+      score = do_Draw(g.opponent_move)
+    else if (g.target_outcome == 'L')
+      score = do_Lose(g.opponent_move)
+    else
+      println(s"****** UNRECOGNIZED TARGET ${g.target_outcome}")
+
+    println(s"${g.line_num} --> ${g.opponent_move},${g.target_outcome} = <to> + ${score}")
+    accumulator ! score
+
+  }
+
+  private def do_Win(opponent_move: Int): Int = {
+    accumulator ! 6 //WE know we are going to win, so send the win total
+    //Rock = 1, paper = 2, scissors = 3
+    if (opponent_move == 1)
+      2 //paper beats rock
+    else if (opponent_move == 2)
+      3 //scissors beats paper
+    else if (opponent_move == 3)
+      1 //rock beats scissors
+    else {
+      println(s"BAD OPPONENT MOVE ${opponent_move}");
+      -1
+    }
+  }
+
+  private def do_Draw(opponent_move: Int): Int = {
+    accumulator ! 3 //WE know we are going to tie , so send the draw total
+    opponent_move //Draws will always be the same as opponent move
+  }
+
+  private def do_Lose(opponent_move: Int): Int = {
+    accumulator ! 0 //WE know we are going to tie , so send the draw total
+    //Rock = 1, paper = 2, scissors = 3
+    if (opponent_move == 1)
+      3 //scissors loses to rock
+    else if (opponent_move == 2)
+      1 //rock loses to paper
+    else if (opponent_move == 3)
+      2 // paper loses to scissors
+    else {
+      println(s"BAD OPPONENT MOVE ${opponent_move}");
+      -1
+    }
+  }
+}
 class Formatter(accumulator:ActorRef) extends Actor{
-
-
   override def receive: Receive = {
     case s:(String,Int) => r_GameString(s)
   }
@@ -85,19 +142,18 @@ class Formatter(accumulator:ActorRef) extends Actor{
       .replace('A','1')
       .replace('B','2')
       .replace('C','3')
-    val suggested_move = chars(1)
-      .replace('X', '1')
-      .replace('Y', '2')
-      .replace('Z', '3')
+    val target_outcome = chars(1)
+      .replace('X', 'L')
+      .replace('Y', 'D')
+      .replace('Z', 'W')
 
     //Rip the value and send for evaluation
-    val suggested_move_value = suggested_move.toInt;
-    //println(s"line ${gs} --> Suggested move ${suggested_move} score is ${suggested_move_value}");
-    accumulator ! suggested_move_value
+    val desired_outcome = target_outcome.charAt(0)
 
     //Pass the evaluation of the game outcome
-    val oc= context.actorOf(Outcome_Evaluator.props(accumulator))
-    oc! Game(opponent_move = opponent_move.toInt, suggested_move =suggested_move_value,gs._2)
+    val od= context.actorOf(Outcome_Decider.props(accumulator))
+    od ! Game(opponent_move.toInt, desired_outcome,gs._2)
+    //println(s"SENT OD: ${opponent_move},${desired_outcome} --> ${gs._2}")
 
   }
 }
