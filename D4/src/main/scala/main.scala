659e848b73020e3dee4679e2138e63308f6b2d77
Bennett Lewis <bennettwlewis@hotmail.com>
2022-12-28 15:46:39 -0600

    that is part 1

--- D4/src/main/scala/main.scala
+++ D4/src/main/scala/main.scala
@@ -1,7 +1,56 @@
 import akka.actor.{Actor, ActorRef, ActorSystem, PoisonPill, Props, actorRef2Scala}
 
-import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
+/**
+ * 1. Split ranges into case object
+ * 2. Send to comparitor for evaluation
+ * 3. if range encapsuated, accumulate
+ */
+
+case class Range_String(range_1:String,range_2:String)
+case class Range_Set(range_1: (Int,Int),range_2:(Int,Int))
+
+object Formatter{
+  def props(accumulator:ActorRef,id:Int):Props = Props( new Formatter(accumulator,id))
+}
+
+class Formatter(accumulator:ActorRef,id:Int) extends Actor{
+
+  def r_RangeString(range_string: Range_String): Unit = {
+    println(s"Actor ${id} received: ${range_string.range_1} vs ${range_string.range_2}")
+
+    //Grab range 1 integers
+    val lower_1 = range_string.range_1.split('-')(0).toInt
+    val upper_1 = range_string.range_1.split('-')(1).toInt
+
+    //grab range 2 integers
+    val lower_2 = range_string.range_2.split('-')(0).toInt
+    val upper_2 = range_string.range_2.split('-')(1).toInt
+
+    //Ship it!
+    self ! Range_Set((lower_1,upper_1),(lower_2,upper_2))
+  }
+
+  def r_RangeSet(range_set: Range_Set): Unit = {
+    val lower_A = range_set.range_1._1
+    val upper_A = range_set.range_1._2
+    val lower_B = range_set.range_2._1
+    val upper_B = range_set.range_2._2
+
+    //If one range encapsulates the other, send a tally
+    if(lower_A <= lower_B && upper_A >= upper_B)
+      accumulator ! 1
+     else if (lower_B <= lower_A && upper_B >= upper_A)
+      accumulator ! 1
+    else
+      accumulator ! 0
+  }
+
+  override def receive: Receive = {
+    case r:Range_String => r_RangeString(r)
+    case rs:Range_Set => r_RangeSet(rs)
+
+  }
+}
 
 
 object main{
@@ -12,24 +61,20 @@ object main{
 
 
     //Parse the input
-    val fileName = "./D3/d3.txt"
-    var bufferedSource = scala.io.Source.fromFile(fileName)
-
+    val fileName = "./D4/d4.input.txt"
     println("READING LINES")
-    var lines = bufferedSource.getLines()
-    //Accumulator
-    val accumulator = system.actorOf(Accumulator.props(300))
-    //bufferedSource.close();
 
     //reset the source....
-    //bufferedSource = scala.io.Source.fromFile(fileName)
+    var bufferedSource = scala.io.Source.fromFile(fileName)
+    val accumulator = system.actorOf(Accumulator.props(1000))
 
     //Foreach line
-    var line_num = 0;
+    var line_num = 1;
     for (line <- bufferedSource.getLines()) {
+      val ranges = line.split(',')
+      val comparitor = system.actorOf(Formatter.props(accumulator,line_num))
+      comparitor ! Range_String(ranges(0),ranges(1))
       //println(s"Line ${line_num} --> ${line}")
-      val comparitor = system.actorOf(Compartmentalizer.props(accumulator))
-      comparitor ! Sack(line,"",line_num)
       line_num = line_num + 1
     }
 
