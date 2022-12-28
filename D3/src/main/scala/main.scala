13577fb94e495b7f4f389a874fda8eb804d2b31a
Bennett Lewis <bennettwlewis@hotmail.com>
2022-12-27 21:57:31 -0600

    Part 1 done

--- D3/src/main/scala/main.scala
+++ D3/src/main/scala/main.scala
@@ -14,37 +14,40 @@ object Compartmentalizer{
  * 1. split into 2 compartments (half of each string)
  * 2. convert to priority [a-z] -> [1-26] [A-Z] -> [27-52]
  */
-class Compartmentalizer(evaluator:ActorRef) extends Actor{
+class Compartmentalizer(accumulator:ActorRef) extends Actor{
 
   private def prioritize(items: String): List[Int] = {
-    val as_priority:ListBuffer[Int] = new ListBuffer[Int]
+    var as_priority: Set[Int] = Set()
     items.chars().forEach(char => {
-      val priority:Int = char.toInt //Ascii value of char
+      val priority:Int = char //Ascii value of char
       var appendMe:Int = 0
 
       //Handle CAPS then Non Caps
+      //A = 65, a = 97
       if(priority >= 65 && priority <= 90)
         appendMe = priority - 38
        else if (priority >= 97 && priority <= 122)
         appendMe = priority - 96
 
-      as_priority.append(appendMe)
 
-      println(s"TO_INT takes ${char} and makes it ${appendMe}");
-      as_priority.append(priority)
+      //as_priority.append(appendMe)
+      as_priority = as_priority + appendMe
     })
 
     as_priority.toList
   }
   private def r_Sack(sack: Sack): Unit ={
-    val sack_split = sack.comp_1.splitAt((sack.comp_1.length/2) - 1) //split in the middle.
+
+    val sack_split = sack.comp_1.splitAt(sack.comp_1.length/2) //split in the middle.
 
     val compartment_1 = prioritize(sack_split._1) //comp 1/2
     val compartment_2 = prioritize(sack_split._2) //comp 2/2
 
-    val comparitor= context.actorOf(Comparitor.props(evaluator)) //create evaluator to compare the halves
+   // create comparitor
+    val comparitor = context.actorOf(Comparitor.props(accumulator));
+
+    //accumulator ! Priority_Sack(compartment_1,compartment_2,sack.sack_id)
     comparitor ! Priority_Sack(compartment_1,compartment_2,sack.sack_id)
-    self ! PoisonPill
   }
 
   override def receive: Receive = {
@@ -61,8 +64,12 @@ object Comparitor{
  */
 class Comparitor(accumulator:ActorRef) extends Actor{
 
-  def r_Sack(sack: Priority_Sack): Unit = {
+  private def r_Sack(sack: Priority_Sack): Unit = {
+    val similarity = sack.comp_1.filter(item => sack.comp_2.contains(item))
+    //val as_chars = similarity.map(priority => priority.toChar)
+    println(s"Similarity of ${sack.sack_id} = ${similarity}")
 
+    accumulator ! similarity.sum
   }
 
   override def receive: Receive = {
@@ -70,12 +77,53 @@ class Comparitor(accumulator:ActorRef) extends Actor{
   }
 }
 
+
+object Accumulator{
+  def props(num_sacks:Int):Props = Props(new Accumulator(num_sacks))
+}
+
 /**
  * 4. sum priorities of duplicates per line
  * 5. sum those sums?
  */
-class Accumulator extends Actor{
-  override def receive: Receive = ???
+class Accumulator(num_sacks:Int) extends Actor{
+
+  private var compartment_1: ListBuffer[Int] = new ListBuffer[Int]()
+  private var compartment_2: ListBuffer[Int] = new ListBuffer[Int]()
+  private val ids:ListBuffer[Int] = ListBuffer[Int]()
+  private var total: ListBuffer[Int] = new ListBuffer[Int]()
+
+  private def r_Sack(sack: Priority_Sack):Unit = {
+
+    if(sack.comp_1.nonEmpty)
+      compartment_1 = compartment_1 ++ sack.comp_1
+
+    if(sack.comp_2.nonEmpty)
+      compartment_2 = compartment_2 ++ sack.comp_2
+
+    ids.append(sack.sack_id)
+
+    if( ids.length == num_sacks ) {
+      println(s"BEGINNING THE TALLY: ${compartment_2.length} and ${compartment_2.length}")
+
+      val similar = compartment_1.filter(item => compartment_2.contains(item))
+      println(s"sum ${similar.sum}")
+    } else {
+      println(s"Handled sack ${sack.sack_id}")
+    }
+  }
+
+  def r_Sum(sub_total: Int): Unit = {
+    println(s"received: ${sub_total}")
+    total.append(sub_total)
+    if(total.length == num_sacks)
+      println(s"SUM IS: ${total.sum} of ${total}")
+  }
+
+  override def receive: Receive = {
+    case s:Priority_Sack => r_Sack(s)
+    case sum:Int => r_Sum(sum)
+  }
 }
 /**
  * TODO
@@ -92,12 +140,19 @@ object main{
     // Start the system
     val system = ActorSystem("SYS")
 
-    //Accumulator
-    val accumulator = system.actorOf(Props[Accumulator]())
 
     //Parse the input
     val fileName = "./D3/d3.txt"
-    val bufferedSource = scala.io.Source.fromFile(fileName)
+    var bufferedSource = scala.io.Source.fromFile(fileName)
+
+    println("READING LINES")
+    var lines = bufferedSource.getLines()
+    //Accumulator
+    val accumulator = system.actorOf(Accumulator.props(300))
+    //bufferedSource.close();
+
+    //reset the source....
+    //bufferedSource = scala.io.Source.fromFile(fileName)
 
     //Foreach line
     var line_num = 0;
@@ -105,9 +160,9 @@ object main{
       //println(s"Line ${line_num} --> ${line}")
       val comparitor = system.actorOf(Compartmentalizer.props(accumulator))
       comparitor ! Sack(line,"",line_num)
-      line_num = line_num + 1;
-
+      line_num = line_num + 1
     }
+
 
     bufferedSource.close()
   }
