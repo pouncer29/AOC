ead8ddb42e11d9c01152fe3417c7fbeda077dcd4
Bennett Lewis <bennettwlewis@hotmail.com>
2023-10-29 21:13:53 -0600

    Commenting the Delegator

--- D5/src/main/scala/d5.scala
+++ D5/src/main/scala/d5.scala
@@ -23,34 +23,91 @@ object Crate_Stack{
   def props(stack:ListBuffer[Char],stacks:Array[ActorRef]) = Props(new Crate_Stack(stack,stacks))
 }
 
+/**
+ * The Delegator class simply delegates tasks to each of the 9 cranes that manage crate stacks.
+ * It maintains 2 queues the "command_queue" of all commands in order, and the "retry_queue" which
+ * holds any move-order that couldn't be executed.
+ * @param rows This is a list of the stacks (List of List of chars)
+ * @param command_tuples The original set of commands, in order
+ */
 class Delegator(rows:ListBuffer[ListBuffer[Char]],command_tuples:ListBuffer[(Int,Int,Int,Int)]) extends Actor{
   val logger:Logger = LoggerFactory.getLogger(Delegator.getClass)
+
+  /**
+   * Holds whether or not a stack "isLocked", that is, has a command being executed on it
+   */
   private var locks = new Array[Boolean](0)
+
+  /**
+   * Holds the ActorRef of each of the stack actors (cranes)
+   */
   private var stacks= new Array[ActorRef](0)
+
+  /**
+   * The Tails of each stack (last item) as this is what the question demands
+   */
   private var tails= new Array[Char](0)
+
+  /**
+   * The # of tail reports we should expect to receive
+   */
   private var tail_count = (rows.length - 1)
+
+  /**
+   * The queue of commands to retry
+   */
   private var retry_queue = new mutable.PriorityQueue[Command]()(Ordering.by(compare))
+
+  /**
+   * The queue of commands as they were read from the instructions file
+   */
   private var command_queue = new mutable.PriorityQueue[Command]()(Ordering.by(compare))
 
+  /**
+   * Because commands are ordered from 0 - N, and vanilla comparitor says that
+   * higher # --> Greater priority, we negate the priority to ensure 0 is the
+   * greatest priority
+   * @param c A "Move Command" who's priority field we need
+   * @return the negated command priority
+   */
   private def compare(c:Command) = c.priority * -1
 
+  /**
+   * Checks the locks relevant to a passed command to see if it can be carried out
+   * @param c A "Move Command" with locks we need to checked
+   * @return True if either stack is locked
+   */
   private def check_lock(c:Command):Boolean = {
+    //Offset 1 for instruction naming convention starting stacks at 1
     val is_locked = !(locks(c.source-1) || locks(c.dest-1))
     println(s"Checking Lock: ${locks(c.source-1)} ${locks(c.dest-1)} --> ${is_locked}")
     is_locked
   }
 
-  private def unlock(c:Unlock): Unit = {
-    println(s"r_ack: unlocked ${c.id}")
-    locks(c.id -1) = false
+  /**
+   * Unlock the stack that sent us the unlock message by setting it's lock to false
+   * @param stack - the stack which is now free
+   */
+  private def unlock(stack:Unlock): Unit = {
+    println(s"r_ack: unlocked ${stack.id}")
+    locks(stack.id -1) = false
   }
 
+  /**
+   * Lock the stacks that pertain to the command
+   * @param c - the command which we are going to execute, and who's
+   *          stacks we must lock
+   */
   private def lock(c:Command): Unit = {
     println(s"locked ${c.source} && ${c.dest}")
     locks(c.source - 1) = true
     locks(c.dest - 1) = true
   }
 
+  /**
+   * receives a tail report, records it, and if we have received all tails, we die.
+   * @param tr - A Tail repport from a stack actor that has been asked to report tails
+   */
   def r_TailReport(tr: Tail_Report): Unit = {
     tails(tr.id -1) = tr.tail
     println(s"RECEIVED TAIL COUNT: ${tr}: ${tail_count}")
@@ -61,11 +118,21 @@ class Delegator(rows:ListBuffer[ListBuffer[Char]],command_tuples:ListBuffer[(Int
     tail_count = tail_count - 1
   }
 
+  /**
+   * Handles coordinating received messages
+   * @return
+   */
    def receive: Receive = {
     case ack:Unlock => unlock(ack)
     case tail:Tail_Report => r_TailReport(tail)
     case "DISTRIBUTE" => distribute_commands()
   }
+
+  /**
+   * Sets up the delegator with locks, stacks, and tails as appropriately
+   * sized arrays and enqueues each of the commands before
+   * setting up the stack actors (col actors)
+   */
   override def preStart(): Unit = {
     super.preStart()
     val num_stacks = rows.last.length
@@ -228,14 +295,22 @@ class Crate_Stack(stack:ListBuffer[Char],stacks:Array[ActorRef]) extends Actor{
 
 
 object d5{
-  private def create_command_tuple(id:Int, command_string:String):(Int,Int,Int,Int)= {
+  /**
+   * Creates a command tuple and assigns it a priority
+   *
+   * @param priority       - integer priority of the command where lower # --> Higher priority
+   * @param command_string - the "command" from the file in the form "move <quant> from <src> to <dest>
+   * @return a tuple in the form <priority,quant,src,dest>
+   */
+  private def create_command_tuple(id: Int, command_string: String): (Int, Int, Int, Int) = {
     val params = command_string.split(" ")
-    val tuple = (id,params(1).toInt,params(3).toInt,params(5).toInt)
+    val tuple = (id, params(1).toInt, params(3).toInt, params(5).toInt)
     //println(s"Created ${tuple}")
     tuple
 
   }
-  def main(args:Array[String]): Unit = {
+
+  def main(args: Array[String]): Unit = {
 
     // Start the system
     val system = ActorSystem("SYS")
@@ -248,12 +323,15 @@ object d5{
     //reset the source....
     var bufferedSource = scala.io.Source.fromFile(fileName)
 
-    //Foreach line
+    // Basically transpose d5.crates as a list of chars indexed by
+    // crate ID [1-9]
     var line_num = 1;
     val rows = ListBuffer[ListBuffer[Char]]()
     for (line <- bufferedSource.getLines()) {
       val crates: ListBuffer[Char] = new ListBuffer[Char]
-      for (i <- 1 until line.length by 4){
+      //Each crate is 4 characters apart (i.e. [A] [B] [C])
+      for (i <- 1 until line.length by 4) {
+        //append the char that represents the crate stack for the
         crates.append(line.charAt(i))
       }
       rows.append(crates)
@@ -266,22 +344,23 @@ object d5{
     fileName = "./D5/d5.instructions"
     println("READING Instructions")
 
-    //reset the source....
+    //reset the source to instructions
     bufferedSource = scala.io.Source.fromFile(fileName)
 
-    val commands = ListBuffer[(Int,Int,Int,Int)]()
+    // Convert the command text to a tuple of ints (PRIORITY (ORDER),QUANT,SRC,DST)
+    val commands = ListBuffer[(Int, Int, Int, Int)]()
     //Foreach line
-    line_num = 1;
+    line_num = 1; //We start at 1 because this is how it is known in the instructions. Each line is a stack
     for (line <- bufferedSource.getLines()) {
       //println(s"${line_num} --> ${line} row")
-      commands += create_command_tuple(line_num,line)
+      commands += create_command_tuple(line_num, line)
       line_num = line_num + 1
     }
 
-    val delegator = system.actorOf(Delegator.props(rows,commands),"Delegator")
+    //Create the Delegator
+    val delegator = system.actorOf(Delegator.props(rows, commands), "Delegator")
 
 
     bufferedSource.close()
   }
-
 }
